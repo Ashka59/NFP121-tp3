345a481bbdfca414a5c49e9bc17f8019ddc8d76d
Hashka <hugo.krysztof@gmail.com>
2021-03-01 20:12:47 +0100

    Modifications question 1

--- src/question1/Cac40.java
+++ src/question1/Cac40.java
@@ -6,21 +6,10 @@ import java.util.List;
 public class Cac40  extends Observable{
 
     private int value;
-    private List<Broker> brokers = new ArrayList<>();
-
-    public void add(Broker broker) {
-        this.brokers.add(broker);
-        setObserverCount(brokers.size());
-    }
-
-    public void remove(Broker broker) {
-        this.brokers.remove(broker);
-        setObserverCount(brokers.size());
-    }
 
     public void setValue(int value) {
         this.value = value;
-        for (Broker broker : this.brokers) {
+        for (Broker broker : getBrokers()) {
             broker.update();
         }
     }
--- src/question1/Observable.java
+++ src/question1/Observable.java
@@ -1,9 +1,28 @@
 package question1;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Observable {
 
-    private int ObserverCount;
-    public int getObserverCount() { return ObserverCount; }
-    public void setObserverCount(int observerCount) { ObserverCount = observerCount; }
+
+    private int observerCount;
+    private List<Broker> brokers = new ArrayList<>();
+
+    public int getObserverCount() { return observerCount; }
+    public void setObserverCount(int observerCount) { this.observerCount = observerCount; }
+
+    public List<Broker> getBrokers() { return brokers; }
+
+    public void add(Broker broker) {
+        this.brokers.add(broker);
+        setObserverCount(brokers.size());
+    }
+
+    public void remove(Broker broker) {
+        this.brokers.remove(broker);
+        setObserverCount(brokers.size());
+    }
+
 }
